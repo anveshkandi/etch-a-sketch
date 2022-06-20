9c9406dbca6b1f7b6a1628abf510a20983ff8599
mynameisanv <kandianvesh@gmail.com>
2022-06-20 18:42:36 -0500

    Cleaned up grid generation, added grid changing functionality

--- script.js
+++ script.js
@@ -1,53 +1,66 @@
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = '#000000';
+
 let sizeBtn = document.getElementById('size-btn');
 let clrBtn = document.getElementById('clr-btn');
 let eraserBtn = document.getElementById('eraser-btn');
-let gridSize = 12;
-let boxWidth = 100 / gridSize;
-console.log(boxWidth);
+let colorBtn = document.getElementById('color-btn');
+let gridSize = DEFAULT_SIZE;
+let penColor = DEFAULT_COLOR;
 
 const gridContainer = document.querySelector('.grid');
-gridContainer.style.backgroundColor = '#FFFFFF';
 
-gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+function createGrid(gridSize){
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
     for (let i = 0; i < gridSize ** 2; i++){
         const div = document.createElement('div');
         div.style.outline = '1px solid';
-        div.setAttribute('class', 'grid-item');
+        div.classList.add('grid-item');
         div.setAttribute('draggable', false);
+        div.addEventListener('mouseover', changeColor);
         gridContainer.appendChild(div);
+        div.style.backgroundColor = '#FFFFFF'; //To avoid creating a double border
     }
+}
 
-let gridItems = document.querySelectorAll('.grid-item');
-let penColor = '#000000';
-// gridItems.forEach(e => {
-//     e.style.backgroundColor = '#FFFFFF';
-// });
 
-gridItems.forEach(e => {
-    e.addEventListener('mouseenter', () => {
-        console.log(e);
-        e.style.backgroundColor = penColor;
-    })
-})
+//Handles the coloring of each grid item
+function changeColor(e){
+    e.target.style.backgroundColor = penColor;
+}
 
-clrBtn.addEventListener('click', () => {
+function clearGrid(){
+    let gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach(e => {
         e.style.backgroundColor = '#FFFFFF';
     })
-});
+}
 
+function eraseGrid(){
+    gridContainer.innerHTML = '';
+}
+
+function changeGrid(gridSize){
+    eraseGrid();
+    createGrid(gridSize);
+}
+
+//Clears the grid and returns to a clear white canvas
+clrBtn.addEventListener('click', clearGrid);
+
+//Changes the grid size and loads a new one with the updated grid size
 sizeBtn.addEventListener('click', ()=>{
     gridSize = parseInt(window.prompt("Enter a grid size!", ""));
-    gridItems.forEach(e => {
-        gridContainer.removeChild(e);
-    })
-    createGrid(gridSize);
+    changeGrid(gridSize);
 })
 
-eraserBtn.addEventListener('click', () => penColor = '#FFFFFF');
 
-function rangeSlide(value) {
-    document.getElementById('slideOutput').innerHTML = value;
+//Testing will update in the near future
+eraserBtn.addEventListener('click', () => penColor = '#FFFFFF');
+colorBtn.addEventListener('click', () => penColor = '#000000');
+
+window.onload = () => {
+    createGrid(DEFAULT_SIZE);
 }
