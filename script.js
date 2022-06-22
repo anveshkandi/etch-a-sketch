1240256ffcca8841262e60636482091776480fa7
mynameisanv <kandianvesh@gmail.com>
2022-06-21 19:28:30 -0500

    Final commit. Added color picker. Cleaned up functionality

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const DEFAULT_SIZE = 16;
 const DEFAULT_COLOR = '#000000';
 
 const gridContainer = document.querySelector('.grid');
-const gridSizeOutput = document.querySelectorAll('.slideOutput')
+const gridSizeOutput = document.querySelectorAll('.slideOutput');
+const colorPreview = document.querySelector('.color-preview');
 
 let sizeBtn = document.getElementById('size-btn');
 let clrBtn = document.getElementById('clr-btn');
 let eraserBtn = document.getElementById('eraser-btn');
-let colorBtn = document.getElementById('color-btn');
 let greyBtn = document.getElementById('grey-btn');
 let rainbowBtn = document.getElementById('rand-btn');
+let colorInput = document.getElementById('color-input');
 let gridSize = DEFAULT_SIZE;
 let penColor = DEFAULT_COLOR;
 let mousedown = false;
@@ -44,8 +45,9 @@ function createGrid(gridSize){
 
 //Handles the coloring of each grid item
 function changeColor(e){
+    if (e.type === 'mouseover' && !mousedown) return;
     //Darkens the cell every time it is colored
-    if(greyscaleMode && mousedown){
+    if(greyscaleMode){
         //Retrieving target rgb to darken it
         color = e.target.style.getPropertyValue("background-color");
         color = color.substring(color.indexOf('(')+1, color.indexOf(')'));
@@ -59,7 +61,7 @@ function changeColor(e){
         e.target.style.backgroundColor = 'rgb(' + [r,b,g].join(',')+ ')';
     }
     //Fills the cell with a random color when it is hovered
-    else if(rainbowMode && mousedown){
+    else if(rainbowMode){
         let r = Math.floor(Math.random()*255);
         let b = Math.floor(Math.random()*255);
         let g = Math.floor(Math.random()*255);
@@ -67,15 +69,14 @@ function changeColor(e){
         e.target.style.backgroundColor = 'rgb(' + [r,b,g].join(',')+ ')';
     }
     //Fills the cell with a user selected color
-    else if (mousedown){
+    else {
         e.target.style.backgroundColor = penColor;
     }
-    else {return;}
 }
 
 //Clears the canvas of all colors
 function clearGrid(){
-    penColor = '#000000';
+    penColor = colorInput.value;
     let gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach(e => {
         e.style.backgroundColor = '#FFFFFF';
@@ -136,8 +137,20 @@ eraserBtn.addEventListener('click', () => {
     greyscaleMode = false; //Toggles other modes off
     rainbowMode = false;
 });
-colorBtn.addEventListener('click', () => penColor = '#000000');
 
+//Returns to color mode when color preview is clicked
+colorPreview.addEventListener('click', () => {
+    greyscaleMode = false;
+    rainbowMode = false;
+})
+
+//Changes pen color and color preview when user chooses new input
+colorInput.addEventListener('change', () => {
+    penColor = colorInput.value;
+    colorPreview.style.backgroundColor = colorInput.value;
+})
+
+//Generating default grid on window load
 window.onload = () => {
     createGrid(DEFAULT_SIZE);
 }
